6314d706c7eaf59712376b7b91faa17a448436d8
Dan Trueman <dtrueman@princeton.edu>
2025-07-24 15:54:13 -0400

    added BKMultiSlider stuff to BKSliders

--- source/interface/components/BKComponents/BKSliders.h
+++ source/interface/components/BKComponents/BKSliders.h
@@ -11,6 +11,281 @@
 #include <chowdsp_plugin_state/chowdsp_plugin_state.h>
 
 // ******************************************************************************************************************** //
+// **************************************************  BKSubSlider **************************************************** //
+// ******************************************************************************************************************** //
+
+class BKSubSlider : public juce::Slider
+{
+public:
+
+    BKSubSlider (SliderStyle sstyle, double min, double max, double def, double increment, int width, int height);
+    ~BKSubSlider();
+
+    double getValueFromText    (const juce::String & text ) override;
+    bool isActive() { return active; }
+    void isActive(bool newactive) {active = newactive; }
+    void setMinMaxDefaultInc(std::vector<float> newvals);
+    void setSkewFromMidpoint(bool sfm);
+
+private:
+
+    double sliderMin, sliderMax;
+    double sliderDefault;
+    double sliderIncrement;
+
+    int sliderWidth, sliderHeight;
+
+    bool sliderIsVertical;
+    bool sliderIsBar;
+    bool skewFromMidpoint;
+
+    bool active;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BKSubSlider)
+};
+
+
+
+
+// ******************************************************************************************************************** //
+// **************************************************  BKMultiSlider ************************************************** //
+// ******************************************************************************************************************** //
+
+/*
+
+ BKMultiSlider is a horizontal array of sliders.
+
+ Each "slider" can have multiple sliders, facilitating, for instance, multiple transposition values
+
+ The user can drag across the sliders to quickly set values
+
+ The user can also enter slider values directly via a text editor:
+    "1 [-1, -2, -3] 2 3" will create three sliders, and the second will have three overlaid sliders
+
+ By default it has 12 sliders, but that can be increased by adding additional values via text entry
+
+ Gaps in the slider array are possible, and indicated by a forward slash '/' in the text editor
+
+ The API includes functions for setting the slider, either all at once (via text) or by individual slider (via mouse)
+ and for deactivating individual sliders or sections of sliders.
+
+*/
+
+class BKMultiSlider :
+    public juce::Component,
+    public juce::Slider::Listener,
+    public juce::TextEditor::Listener,
+    public juce::ImageButton::Listener
+{
+
+public:
+
+    // *** Public Instance Methods *** //
+
+    // constructor
+    BKMultiSlider();
+    ~BKMultiSlider();
+
+    // when the client sends an array of only the active slider values, this will construct the complete array
+    // of slider values, including inactive sliders, and then call setTo. So
+    //      newActiveVals = {1, 2, 3}
+    //      newactives = {true, false, false, true, true, false....... false}
+    // will result in [1 / / 2 3 / / / / / / /], where / represents an inactive slider (gap)
+    void setToOnlyActive(juce::Array<juce::Array<float>> newActiveVals, juce::Array<bool> newactives, juce::NotificationType newnotify);
+
+    // as above, but takes a 1d array, for sliders that don't use subSliders
+    void setToOnlyActive(juce::Array<float> newActiveVals, juce::Array<bool> newactives, juce::NotificationType newnotify);
+
+    // identifiers
+    void setName(juce::String newName){
+        sliderName = newName;
+        showName.setText(sliderName, juce::dontSendNotification);
+    }
+    juce::String getName() { return sliderName; }
+    void setToolTipString(juce::String newTip) { showName.setTooltip(newTip); bigInvisibleSlider->setTooltip(newTip); }
+
+    // slider range and skew
+    void setMinMaxDefaultInc(std::vector<float> newvals);
+    void setSkewFromMidpoint(bool sfm);
+
+    // highlight whichever slider is currently active in bK
+    void setCurrentSlider(int activeSliderNum);
+    void deHighlightCurrentSlider(void);
+    inline int getNumVisible(void) const noexcept { return allSliderVals.size(); } //return numVisibleSliders;}
+
+    // should this slider allow subsliders? (multiple transpositions, for instance)
+    void setAllowSubSlider(bool ss) { allowSubSliders = ss; }
+
+    // name of the subslider (add transposition, for instance)
+    void setSubSliderName(juce::String ssname) { subSliderName = ssname; }
+
+    // listeners overrride these functions so they can get what they need from MultiSlider
+    // Multislider will call these when values change, so the clients can update
+    class Listener
+    {
+    public:
+
+        virtual ~Listener() {};
+        virtual void multiSliderValueChanged(juce::String name, int whichSlider, juce::Array<float> values) = 0;
+        virtual void multiSliderAllValuesChanged(juce::String name, juce::Array<juce::Array<float>> values, juce::Array<bool> states) = 0;
+    };
+
+    // listeners
+    juce::ListenerList<Listener> listeners;
+    void addMyListener(Listener* listener)     { listeners.add(listener);      }
+    void removeMyListener(Listener* listener)  { listeners.remove(listener);   }
+
+
+private:
+
+    // *** Private Instance Methods *** //
+
+    // this is the main function for setting the multislider
+    // the array of arrays is the slider values, with the inner array for subsliders (multiple sliders at one position
+    // the array of bools sets which of the sliders is actually active
+    void setTo(juce::Array<juce::Array<float>> newvals, juce::Array<bool> newactives, juce::NotificationType newnotify);
+
+    // initialize the slider; it should have no less than numDefaultSliders, all set to sliderDefault value
+    void initializeSliderVals(int howmany);
+
+    // update and activate a particular slider
+    void updateSliderVal(int which, int whichSub, float val);
+
+    // print out the current vals, for debugging
+    void printSliderVals();
+
+    // draw the actual sliders
+    void drawSliders(juce::NotificationType newnotify);
+
+    // return all values, including inactive sliders
+    juce::Array<juce::Array<float>> getAllValues() { return allSliderVals; }
+
+    // return only those sliders that are active
+    juce::Array<juce::Array<float>> getAllActiveValues();
+
+    // return the values for one slider
+    juce::Array<float> getOneSliderBank(int which);
+
+    // inserts a slider into the multislider
+    void addSlider(int where, bool active, juce::NotificationType newnotify);
+
+    // adds an additional slider at a particular index; it's possible to have multiple
+    // sliders at one location, facilitating, for instance, multiple transpositions at one time
+    void addSubSlider(int where, bool active, juce::NotificationType newnotify);
+
+    // as above, but updates the allSliderVals and whichSlidersActive first
+    void addActiveSubSlider(int where, juce::NotificationType newnotify);
+
+    // these methods clear currently active sliders
+    // exception: you can't clear the FIRST slider, as we need at least one active slider
+    void deactivateSlider(int where, juce::NotificationType notify);
+    void deactivateAll(juce::NotificationType notify);
+    void deactivateAllAfter(int where, juce::NotificationType notify);
+    void deactivateAllBefore(int where, juce::NotificationType notify);
+
+    // slider values can be edited directly via a text editor
+    // values in brackets will be collected at one index
+    // for example: "0 [1 2 3] 4" will have three active sliders, and the second one will have three subsliders
+    inline void setText(juce::String text) { editValsTextField->setText(text, juce::dontSendNotification); }
+    inline juce::TextEditor* getTextEditor(void) { return editValsTextField.get();}
+    inline void dismissTextEditor(bool setValue = false);
+
+    // identify which slider is mouseDowned
+    int whichSlider (const juce::MouseEvent &e);
+
+    // identify which subSlider is closest to cursor after mouseDown on a particular slider
+    int whichSubSlider (int which);
+
+    // identify which subSlider is closest on mouseOver
+    int whichSubSlider (int which, const juce::MouseEvent &e);
+
+    // update which slider is active after mouseDrag and mouseUp; to support dragging across multiple sliders
+    int whichActiveSlider (int which);
+
+    // for highlighting text related to sliders
+    void highlight(int activeSliderNum);
+    void deHighlight(int sliderNum);
+    int getActiveSlider(int sliderNum);
+
+    // rescale/normalize slider ranges
+    void resetRanges();
+
+    // mouse/text events
+    void mouseDrag(const juce::MouseEvent &e) override;
+    void mouseMove(const juce::MouseEvent& e) override;
+    void mouseDoubleClick (const juce::MouseEvent &e) override;
+    void mouseDown (const juce::MouseEvent &event) override;
+    void mouseUp (const juce::MouseEvent &event) override;
+    void textEditorEscapeKeyPressed (juce::TextEditor& textEditor) override;
+    void sliderValueChanged (juce::Slider *slider) override;
+    void textEditorReturnKeyPressed(juce::TextEditor& textEditor) override;
+    void textEditorFocusLost(juce::TextEditor& textEditor) override;
+    void textEditorTextChanged(juce::TextEditor&) override;
+    void buttonClicked(juce::Button* button) override;
+    void showModifyPopupMenu(int which);
+    static void sliderModifyMenuCallback (const int result, BKMultiSlider* slider, int which);
+
+    void resized() override;
+
+
+    // *** Instance Variables *** //
+
+    // holds all the current values, including for sub sliders (hence the double array)
+    juce::Array<juce::Array<float>> allSliderVals;
+
+    // which of these sliders is active; the first is always true
+    juce::Array<bool> whichSlidersActive;
+
+    // the invisible slider lays on top of all the sliders, its value
+    // is used to set the values of the individual slider that the mouse is nearest
+    double currentInvisibleSliderValue;
+
+    // allow sliders to have multiple values
+    bool allowSubSliders;
+
+    // if allowSubSliders, what to call it "add [subSliderName]"
+    juce::String subSliderName;
+
+    // to keep track of stuff
+    int currentSubSlider;
+    int lastHighlightedSlider;
+    bool focusLostByEscapeKey;
+    bool skewFromMidpoint;
+    float clickedHeight;
+
+    // default values
+    double sliderMin, sliderMax, sliderMinDefault, sliderMaxDefault;
+    double sliderDefault;
+    double sliderIncrement;
+
+    // dimensions
+    int totalWidth;
+    int sliderHeight;
+    float sliderWidth;
+    int displaySliderWidth;
+
+    // by default, how many sliders to show (12)
+    int numDefaultSliders;
+
+    // UI elements
+    BKMultiSliderLookAndFeel activeSliderLookAndFeel;
+    BKMultiSliderLookAndFeel passiveSliderLookAndFeel;
+    BKMultiSliderLookAndFeel highlightedSliderLookAndFeel;
+    BKMultiSliderLookAndFeel displaySliderLookAndFeel;
+    juce::String sliderName;
+    juce::Label showName;
+    juce::Slider::SliderStyle subsliderStyle;
+    juce::OwnedArray<juce::OwnedArray<BKSubSlider>> sliders;
+    std::unique_ptr<BKSubSlider> displaySlider;
+    std::unique_ptr<BKSubSlider> bigInvisibleSlider;
+    std::unique_ptr<juce::TextEditor> editValsTextField;
+    std::unique_ptr<juce::ImageButton> rotateButton;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BKMultiSlider)
+};
+
+
+// ******************************************************************************************************************** //
 // *************************************************  BKStackedSlider ************************************************* //
 // ******************************************************************************************************************** //
 
--- source/synthesis/framework/Processors/BlendronicProcessor.h
+++ source/synthesis/framework/Processors/BlendronicProcessor.h
@@ -12,86 +12,44 @@
 #include <chowdsp_sources/chowdsp_sources.h>
 #include <chowdsp_plugin_state/chowdsp_plugin_state.h>
 
-// TODO change params
-
 struct BlendronicParams : chowdsp::ParamHolder
 {
+    // gain slider params, for all gain-type knobs
+    float rangeStart = -60.0f;
+    float rangeEnd = 6.0f;
+    float skewFactor = 2.0f;
 
     // Adds the appropriate parameters to the Blendronic Processor
     BlendronicParams()
     {
-        add (gainParam, hammerParam, velocityParam, resonanceParam, attackParam,
-             decayParam, sustainParam, releaseParam);
+        add (outputGain, outputSendParam);
     }
 
-    // juce::Gain param
-    chowdsp::GainDBParameter::Ptr gainParam {
-            juce::ParameterID { "gain", 100 },
-            "Gain",
-            juce::NormalisableRange { -30.0f, 0.0f },
-            -24.0f
+    /*
+     * Multisliders for:
+     *      - beat lengths          [0, 8]      => FloatParam multipliers of beat length set by Tempo
+     *      - delay lengths         [0, 8]      => FloatParam multipliers of beat length set by Tempo
+     *      - smoothing             [0, 500]    => ms TimeMsParameter
+     *      - feedback coefficients [0, 1]      => FloatParam
+     */
+
+    // Gain for output send (for other blendronics, VSTs, etc...)
+    chowdsp::GainDBParameter::Ptr outputSendParam {
+        juce::ParameterID { "Send", 100 },
+        "Send",
+        juce::NormalisableRange { rangeStart, rangeEnd, 0.0f, skewFactor, false },
+        0.0f,
+        true
     };
 
-    // Hammer param
-    chowdsp::GainDBParameter::Ptr hammerParam {
-            juce::ParameterID { "hammer", 100 },
-            "Hammer",
-            juce::NormalisableRange { -30.0f, 0.0f }, // FIX
-            -24.0f
+    // for the output gain slider, final gain stage for this prep (meter slider on right side of prep)
+    chowdsp::GainDBParameter::Ptr outputGain {
+        juce::ParameterID { "OutputGain", 100 },
+        "Output Gain",
+        juce::NormalisableRange { -80.0f, rangeEnd, 0.0f, skewFactor, false },
+        0.0f,
+        true
     };
-
-    // Velocity param
-    chowdsp::FloatParameter::Ptr velocityParam {
-            juce::ParameterID { "Velocity", 100 },
-            "Velocity",
-            chowdsp::ParamUtils::createNormalisableRange (20.0f, 20000.0f, 2000.0f), // FIX
-            1000.0f,
-            &chowdsp::ParamUtils::floatValToString,
-            &chowdsp::ParamUtils::stringToFloatVal
-    };
-
-    // Resonance param
-    chowdsp::GainDBParameter::Ptr resonanceParam {
-            juce::ParameterID { "resonance", 100 },
-            "Resonance",
-            juce::NormalisableRange { -30.0f, 0.0f }, // FIX
-            -24.0f
-    };
-
-    // Attack param
-    chowdsp::TimeMsParameter::Ptr attackParam {
-            juce::ParameterID { "attack", 100 },
-            "attack",
-            chowdsp::ParamUtils::createNormalisableRange (2.01f, 10.0f, 4.0f),
-            3.5f,
-    };
-
-    // Decay param
-    chowdsp::TimeMsParameter::Ptr decayParam {
-            juce::ParameterID { "decay", 100 },
-            "Decay",
-            chowdsp::ParamUtils::createNormalisableRange (2.01f, 10.0f, 4.0f), // FIX
-            3.5f,
-    };
-
-    // Sustain param
-    chowdsp::FloatParameter::Ptr sustainParam {
-            juce::ParameterID { "sustain", 100 },
-            "Sustain",
-            chowdsp::ParamUtils::createNormalisableRange (20.0f, 20000.0f, 2000.0f),
-            1000.0f,
-            &chowdsp::ParamUtils::floatValToString,
-            &chowdsp::ParamUtils::stringToFloatVal
-    };
-
-    // Release param
-    chowdsp::TimeMsParameter::Ptr releaseParam {
-            juce::ParameterID { "release", 100 },
-            "Release",
-            chowdsp::ParamUtils::createNormalisableRange (2.01f, 10.0f, 4.0f), // FIX
-            3.5f,
-    };
-
 };
 
 struct BlendronicNonParameterState : chowdsp::NonParamState
