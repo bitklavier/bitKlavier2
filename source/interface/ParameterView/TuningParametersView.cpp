886addf5b3d68c19c4a87fb2b5e8c44c8611ed61
Dan Trueman <dtrueman@princeton.edu>
2026-01-26 22:53:43 -0500

    the setVisible() stuff for the various types of tuning seems to work now

--- source/interface/ParameterView/TuningParametersView.cpp
+++ source/interface/ParameterView/TuningParametersView.cpp
@@ -251,77 +251,33 @@ TuningParametersView::TuningParametersView(
 
 void TuningParametersView::showStaticTuning(bool show)
 {
-    /**
-     * the "setVisible" approach here has run into some openGL asserts, either on
-     * opening or closing the Tuning prep popup. we haven't been able to figure them
-     * out so far; are maybe in the button code, perhaps in render somewhere.
-     * workaround for now is to show all the components, and change their alpha instead
-     * part of the fix was also in PreparationPopup::reset()
-     */
-//    tuning_combo_box->setVisible(show);
-//    fundamental_combo_box->setVisible(show);
-//    absolutekeyboard->setVisible(show);
-//    circular_keyboard->setVisible(show);
-//    semitoneSection->setVisible(show);
-//    offsetKnobSection->setVisible(show);
-
-    /**
-     * the "setAlpha" approach is also a bit of a pain for subsections, leave for later
-     */
-//    if(show)
-//    {
-//        tuning_combo_box->setAlpha(1.);
-//        fundamental_combo_box->setAlpha(1.);
-//        absolutekeyboard->setAlpha(1.);
-//        circular_keyboard->setAlpha(1.);
-//        semitoneSection->setAlpha(1.);
-//        offsetKnobSection->setAlpha(1.);
-//    }
-//    else
-//    {
-//        tuning_combo_box->setAlpha(0.25);
-//        fundamental_combo_box->setAlpha(0.25);
-//        absolutekeyboard->setAlpha(0.25);
-//        circular_keyboard->setAlpha(0.25);
-//        semitoneSection->setAlpha(0.25);
-//        offsetKnobSection->setAlpha(0.25);
-//    }
-//
-//    tuning_combo_box->redoImage();
-//    fundamental_combo_box->redoImage();
-//    absolutekeyboard->redoImage();
-//    circular_keyboard->redoImage();
-//    offsetKnobSection->redoImage();
+    tuning_combo_box->setVisible(show);
+    fundamental_combo_box->setVisible(show);
+    absolutekeyboard->setVisible(show);
+    circular_keyboard->setVisible(show);
+    semitoneSection->setVisible(show);
+    offsetKnobSection->setVisible(show);
 }
 
 void TuningParametersView::showAdaptiveTuning(bool show)
 {
-//    adaptiveSection->setVisible(show);
-//    if(show) adaptiveSection->setAlpha(1.);
-//    else adaptiveSection->setAlpha(0.25);
+    adaptiveSection->setVisible(show);
 }
 
 void TuningParametersView::showSpringTuning(bool show)
 {
-//    if(show)
-//    {
-////        springTuningSection->setVisible (show);
-//        springTuningSection->setAlpha(1.);
-//    }
-//    else
-//    {
-//        springTuningSection->setAlpha(0.25);
-//    }
-
+    springTuningSection->setVisible (show);
     params.tuningState.springTuner->setRate (params.tuningState.springTuningParams.rate->getCurrentValue(), show);
 }
 
 void TuningParametersView::showCurrentTuningType()
 {
-    if(params.tuningState.tuningType->get() == TuningType::Static)
-        showStaticTuning(true);
-    else
-        showStaticTuning(false);
+    // always show static tuning
+    showStaticTuning(true);
+    // if(params.tuningState.tuningType->get() == TuningType::Static)
+    //     showStaticTuning(true);
+    // else
+    //     showStaticTuning(false);
 
     if(params.tuningState.tuningType->get() == TuningType::Adaptive || params.tuningState.tuningType->get() == TuningType::Adaptive_Anchored)
         showAdaptiveTuning(true);
