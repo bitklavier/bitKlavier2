5bb90214d153ca20addb9c536e438daab022ccba
Dan Trueman <dtrueman@princeton.edu>
2026-02-11 23:48:17 -0500

    fixed issue with isToggle in ModulationProcessor with initialization

--- source/modulation/ModulationModuleSection.cpp
+++ source/modulation/ModulationModuleSection.cpp
@@ -32,21 +32,16 @@ ModulesInterface(v), modulation_list_(modulationProcessor), undo (um)
      }
      parent = modulation_list_->getValueTree();
      modulation_list_->addListener(this);
-//    factory.registerType<OscillatorModuleProcessor, juce::ValueTree, LEAF*>("osc");
-//    factory.registerType<FilterModuleProcessor, juce::ValueTree, LEAF*>("filt");
      addListener(m);
 
     // Initialize toggle button and processor state from ValueTree
     const bool vtToggle = (bool) parent.getProperty(IDs::modulationToggleMode, false);
     if (setToggleMode != nullptr)
         setToggleMode->setToggleState(vtToggle, juce::NotificationType::dontSendNotification);
-    // if (modulation_list_ != nullptr && modulation_list_->proc_ != nullptr)
-    //     modulation_list_->proc_->isToggle = vtToggle;
 }
 
 void ModulationModuleSection::modulatorAdded( ModulatorBase* obj)
 {
-//    auto obj = tracktion::engine::getObjectFor(*modulation_list_, v);
     auto *module_section = new ModulationSection(obj->state,obj->createEditor(), undo);
     {
         //TODO : make sure all the addSubsections are getting locked
